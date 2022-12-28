6aed5de0415280dbf23dee87b583a1e310e7571e
germaniko <german.m.pesce@gmail.com>
2022-12-28 20:43:57 -0300

    Save the history in localStorage and update the 'Recently' section

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,20 +1,35 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable} from '@angular/core';
+import { HttpClient, HttpParams} from '@angular/common/http';
+import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
+  private urlService: string = 'https://api.giphy.com/v1/gifs'
   private apiKey: string = 'YNcFPKdO1seIRCPw3D62n3G40ugSrs96';
   private _history: string[] = [];
   // Must change any to correct type
-  public results: any[] = [];
+  public results: Gif[] = [];
 
   get history() {
     return [...this._history];
   }
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    // Option1
+    //this._history = localStorage.getItem('history');
+    // if ( localStorage.getItem('history')) {
+    //   this._history = JSON.parse (localStorage.getItem('history')!);
+    // }
+
+    // Option1
+    this._history = JSON.parse(localStorage.getItem('history')!) || [];
+    
+    //Save results when you refresh the page
+    //this.results = JSON.parse(localStorage.getItem('results')!) || [];
+
+  }
 
   searchGifs(query: string) {
     query = query.trim().toLowerCase();
@@ -22,19 +37,31 @@ export class GifsService {
     if (!this._history.includes(query)) {
       this._history.unshift(query);
       this._history = this._history.splice(0, 10);
-    }
 
+      localStorage.setItem('history', JSON.stringify(this._history));
+    }
+    
     // fetch('https://api.giphy.com/v1/gifs/search?api_key=YNcFPKdO1seIRCPw3D62n3G40ugSrs96&q=dragon ball z')
     // .then( response => {
     //   response.json().then(data => console.log(data))
     // })
+
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', '10')
+    .set('q', query);
+
+
     this.http
-      .get(
-        `https://api.giphy.com/v1/gifs/search?api_key=YNcFPKdO1seIRCPw3D62n3G40ugSrs96&q=${query}&limit=10`
+      .get<SearchGifsResponse>(
+        `${this.urlService}/search`, {params: params}
       )
-      .subscribe((response: any) => {
-        console.log(response);
+      .subscribe((response: SearchGifsResponse) => {
         this.results = response.data;
+
+        //Save results in localStorage
+        //localStorage.setItem('results', JSON.stringify(this.results));
+
       });
   }
 }
